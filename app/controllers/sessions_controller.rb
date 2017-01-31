33ed62fa378f378d2eb51582642cf53c31666c8a
vishal <vishal.i.lad@accenture.com>
2017-01-31 19:35:38 +0530

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user # Compact Redirect: Rails automatically converts this to the route for the user’s profile page:
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
